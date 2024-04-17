da6e2334f51276294a6a40750a633b6544d3b8b2
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-17 21:35:37 +0200

    Add files via upload

--- functions/api.js
+++ functions/api.js
@@ -1,12 +1,8 @@
-import ws from 'ws';
-import serverless from 'serverless-websocket';
+const serverless = require('serverless-http');
 
-const wss = new ws.WebSocketServer({ noServer: true });
+const app = require("http").createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World');
+});
 
-const clients = new Map();
-
-wss.on('connection', (socket) => {
-    socket.send('hello world');
-})
-
-export const handler = serverless(wss);
+exports.handler = serverless(app);
