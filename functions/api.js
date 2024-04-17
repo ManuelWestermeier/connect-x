c7ebed3e2cbdbc3b16fd3709cf0c9a97f0329c72
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-04-17 21:04:19 +0200

    Add files via upload

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,3 @@
-/*
 import ws from 'ws';
 
 const wss = new ws.Server({ port: 3000 });
@@ -6,8 +5,5 @@ const wss = new ws.Server({ port: 3000 });
 wss.on('connection', (socket) => {
     socket.send('hello world');
 })
-*/
 
-export const handler = (req, res) => {
-  res.end("hello world");
-}
+export const handler = wss
